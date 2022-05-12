1afdf520bb579bb79e39415e9a29930e0eb73ccb
Daniil Panov <from4life@pm.me>
2022-05-12 08:28:01 +0700

    Add function that calls for appropriate operator function

--- js-file.js
+++ js-file.js
@@ -1,17 +1,29 @@
 //operator functions
 function add(num1, num2) {
     return num1 + num2;
-  }
+}
   
 function subtract(num1, num2) {
     return num1 - num2;
-  }
+}
   
 function multiply(num1, num2) {
     return num1 * num2;
-  }
+}
   
 function divide(num1, num2) {
     return num1 / num2;
-  }
+}
 
+//take an operator and two number and call for appropriate function above
+function operate(op, num1, num2) {
+    if (op === "add") {
+      return add(num1, num2);
+    } else if (op === "subtract") {
+      return subtract(num1, num2);
+    } else if (op === "multiply") {
+      return multiply(num1, num2);
+    } else if (op === "divide") {
+      return divide(num1, num2);
+    }
+}
