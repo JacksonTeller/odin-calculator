1d2581b15bba3a9ae1c64c37e9e5d4a8ecf7fe7f
Daniil Panov <from4life@pm.me>
2022-05-12 15:07:36 +0700

    Add functions for saving the numbers and values, and for showing the answer

--- js-file.js
+++ js-file.js
@@ -17,20 +17,20 @@ function divide(num1, num2) {
 
 //take an operator and two number and call for appropriate function above
 function operate(op, num1, num2) {
-    if (op === "add") {
+    if (op === 'add') {
       return add(num1, num2);
-    } else if (op === "subtract") {
+    } else if (op === 'subtract') {
       return subtract(num1, num2);
-    } else if (op === "multiply") {
+    } else if (op === 'multiply') {
       return multiply(num1, num2);
-    } else if (op === "divide") {
+    } else if (op === 'divide') {
       return divide(num1, num2);
     }
 }
 
 //display the input
 function showNumbers(e) {
-    // let numDisplay = document.querySelector(".display")
+    // let numDisplay = document.querySelector('.display')
     // console.log(numDisplay.innerText)
     numDisplay.innerText += e.target.innerText
 }
@@ -45,33 +45,31 @@ clearBtn.addEventListener('click', clear);
 
 //main function
 
-function giveSecondValue() {
-  numDisplay.innerText = '';
-  let num2 = numDisplay.innerText;
-  return num2;
-}
-
-//Step1: take the first number and operator
-let multiplyBtn = document.querySelector('.multiply');
-let numDisplay = document.querySelector(".display");
-let equalBtn = document.querySelector('.equal');
-
-const displArr = {};
-console.log(displArr);
-multiplyBtn.addEventListener('click', (e) => {
+function takeOpAndNum1(e) {
   displArr['num1'] = numDisplay.innerText;
   displArr['operator'] = e.target.className.slice(4);
   clear();
   console.log(displArr);
-});
+}
 
-//Step2: take the second number and show the answer
-equalBtn.addEventListener('click', () => {
+function equalsTo() {
   displArr['num2'] = numDisplay.innerText;
   console.log(displArr);
   numDisplay.innerText = operate(displArr['operator'], displArr['num1'], displArr['num2']);
   console.log(operate(displArr['operator'], displArr['num1'], displArr['num2']));
-})
+}
+
+//Step1: take the first number and operator
+let multiplyBtn = document.querySelector('.multiply');
+let numDisplay = document.querySelector('.display');
+let equalBtn = document.querySelector('.equal');
+
+const displArr = {};
+console.log(displArr);
+multiplyBtn.addEventListener('click', takeOpAndNum1);
+
+//Step2: take the second number and show the answer
+equalBtn.addEventListener('click', equalsTo);
 
 
 
@@ -81,8 +79,8 @@ equalBtn.addEventListener('click', () => {
 let ops = document.querySelectorAll('.ops');
 
 
-let numBtns = document.querySelectorAll(".num-btns");
-numBtns.forEach(btn => btn.addEventListener("click", showNumbers));
+let numBtns = document.querySelectorAll('.num-btns');
+numBtns.forEach(btn => btn.addEventListener('click', showNumbers));
 
 // let displayValue = numDisplay.innerText;
 
